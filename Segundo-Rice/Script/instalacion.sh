07c173d3e1e7f77538540453897c90bce5b5aa47
Stilldani <ttuko7111@gmail.com>
2026-02-01 00:26:10 -0600

    Simplify installation script by removing mkdir commands
    Removed unnecessary directory creation commands and added a command to start Hyprland.

--- Segundo-Rice/Script/instalacion.sh
+++ Segundo-Rice/Script/instalacion.sh
@@ -2,11 +2,8 @@
 
 sudo pacman -S hyprland rofi dolphin cava 
 
-mkdir ~/.config/hypr/
-mkdir ~/.config/rofi/
-mkdir ~/.config/Waybar
-mkdir ~/.local/share/rofi
-
+start-hyprland
+sleep 30
 
 cp ~/Linux-config/Segundo-Rice/dotfiles/hyprland/hyprland.conf ~/.config/hypr
 cp ~/Linux-config/Segundo-Rice/dotfiles/waybar/Nikolas-Lehto ~/.config/Waybar/Nikolas-Lehto
